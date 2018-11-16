9c9ed7d7b1c19b73e0c87d931c62db424afb4ec4
bazardeh <42860792+bazardeh@users.noreply.github.com>
2018-11-16 18:59:40 +0330

    Initial commit

---
+++ src/main/java/ir/loginSecuerty/LoginSecuertyApplication.java
@@ -0,0 +1,12 @@
+package ir.loginSecuerty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginSecuertyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginSecuertyApplication.class, args);
+	}
+}
---
+++ src/test/java/ir/loginSecuerty/LoginSecuertyApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.loginSecuerty;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LoginSecuertyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
